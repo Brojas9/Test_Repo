30654ac597424f54c0ec0d9f0a9bb437405a51c4
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-01-30 22:34:28 -0500

    update

--- test_repo/test.cpp
+++ test_repo/test.cpp
@@ -2,9 +2,14 @@
 
 int main(void)
 {
-	for (int counter = 0; counter < 100; counter++)
+	for (int i = 0; i < 100; i++)
 	{
-		printf("Hello, world!\n");
+		printf("Hello, world 2.0 !\n");
 	}
+	if (5 == 10)
+	{
+		printf("Hello Kitchener");
+	}
+
 	return 0;
 }
