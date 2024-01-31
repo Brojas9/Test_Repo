091b3d3a7cfa8384320dbeab87fa0704309ce116
Anthony Moronta <85040800+Anthonyj45@users.noreply.github.com>
2024-01-31 11:02:11 -0500

    Update test.cpp

--- test_repo/test.cpp
+++ test_repo/test.cpp
@@ -3,8 +3,9 @@
 int main(void)
 {
 	int myArray[7] = {15,20,35,75,1,2,3};
+	
 	//Please, write your code here
-	len = sizeof(myArray) / sizeof(myArray[0]));
+	int len = sizeof(myArray) / sizeof(myArray[0]);
 
 	for (int i = 0; i < len; i++)
 	{
