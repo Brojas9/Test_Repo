206fa3422c7901072afb43b27508e60fcf5125d9
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2024-01-30 23:21:37 -0500

    Code complete

--- test_repo/test.cpp
+++ test_repo/test.cpp
@@ -4,6 +4,12 @@ int main(void)
 {
 	int myArray[7] = {15,20,35,75,1,2,3};
 	//Please, write your code here
+	len = sizeof(myArray) / sizeof(myArray[0]));
+
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d\n", myArray[i]);
+	}
 	
 	return 0;
 }
