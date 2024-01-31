8a8f3e9db09d8bd9a90c9dc7593ed38dfe78a3e0
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2024-01-30 22:55:49 -0500

    FizzBuzz

--- test_repo/test.cpp
+++ test_repo/test.cpp
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-	printf("Hello, world!");
+	for (int i = 0; i <= 100; i++)
+	{
+		if (i % 2 == 0)
+		{
+			printf("%d - Fizz", i);
+		}
+		else
+		{
+			printf("%d - Buzz", i);
+		}
+	}
 
 	return 0;
 }
